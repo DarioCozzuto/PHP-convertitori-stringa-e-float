d659cc701f87148f1697b47304411c64ddf0c028
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-10 20:34:42 +0100

    Add files via upload

---
+++ PHP convertitori stringa e float.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+$y=19.2;
+$z="234"; 
+
+//convertire float to int
+$variabile=int($y);
+echo $variabile;
+echo "<br>";
+
+
+//convertire stringa to int
+$variabile=float($z);
+echo $variabile;
+echo "<br>";
+
+
+
+?>
+ 
+</body>
+</html>
